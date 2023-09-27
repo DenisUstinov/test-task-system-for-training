f49c43e22764545fd3e137001baaade8797fe9e5
Denin Ustinov <denis@MacBook.home>
2023-09-27 08:10:45 +0700

    Add "storage" application models

--- trainingsystems/storage/models.py
+++ trainingsystems/storage/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+
+class ProductUser(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    link_to_video = models.URLField()
+    duration = models.DurationField()
+    products = models.ManyToManyField(Product, through='LessonProduct')
+
+
+class LessonProduct(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class LessonView(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    view_time_seconds = models.DurationField(default=0)
+    is_completed = models.BooleanField(default=False)
