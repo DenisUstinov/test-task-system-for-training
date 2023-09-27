aaaa0944aa8bef4539b24b9d1b6213254c05e92f
Denin Ustinov <denis@MacBook.home>
2023-09-27 08:18:40 +0700

    Register models

--- trainingsystems/storage/admin.py
+++ trainingsystems/storage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Owner, Product, ProductUser, Lesson, LessonProduct, LessonView
 
-# Register your models here.
+admin.site.register(Owner)
+admin.site.register(Product)
+admin.site.register(ProductUser)
+admin.site.register(Lesson)
+admin.site.register(LessonProduct)
+admin.site.register(LessonView)
