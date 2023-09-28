bbb568824a9be412aebbb2011274a44fbd90fd0f
Denin Ustinov <denis@MacBook.home>
2023-09-28 13:20:10 +0700

    Add "lesson_product_id" field, delete "lesson" for LessonView

--- trainingsystems/storage/models.py
+++ trainingsystems/storage/models.py
@@ -14,6 +14,9 @@ class ProductUser(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('product', 'user')
+
 
 class Lesson(models.Model):
     title = models.CharField(max_length=255)
@@ -26,15 +29,26 @@ class LessonProduct(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('lesson', 'product')
+
 
 class LessonView(models.Model):
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    lesson_product_id = models.ForeignKey(LessonProduct, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     view_time_seconds = models.IntegerField(default=0)
     is_completed = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        if self.view_time_seconds > self.lesson.duration * 0.8:
+        # Получить связанный объект LessonProduct
+        lesson_product = self.lesson_product_id
+
+        # Получить объект Lesson, связанный с этим LessonProduct
+        lesson = lesson_product.lesson
+
+        if self.view_time_seconds > lesson.duration * 0.8:
             self.is_completed = True
         super(LessonView, self).save(*args, **kwargs)
+
+
